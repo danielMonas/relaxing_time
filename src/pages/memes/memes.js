ed4de4362696d0032af4526e1fbeedd3d652c238
danielMonas <danielmonas.12@gmail.com>
2022-01-02 22:16:33 +0200

    added more memes

--- src/pages/memes/memes.js
+++ src/pages/memes/memes.js
@@ -7,7 +7,28 @@ import Navbar from '../../components/Header/Header';
 export default class Memes extends Component {
   constructor() {
     super();
-    this.ids = ['Rjymxp8', 'bcNtqPw', 'afregsatrw45']
+    this.ids = [
+      'Rjymxp8',
+      'bcNtqPw', 
+      'c8envsP',
+      'mOAbPsF',
+      'uLn3f0i',
+      'rVeThh6',
+      '7Y61Qq6',
+      'VaJNVZ2',
+      'XMrtBSV',
+      'TCsY1Uq',
+      'EZ6so8N',
+      'uwi4amb',
+      'D7J7qKH',
+      'nsticnp',
+      'DH8PFJ7',
+      'ckfvXkw',
+      'k2ifvtU',
+      'x7eO2a5',
+      'W9MkJxT',
+      'ZN0oIQ0',
+    ]
   }
 
   getRandomId = () => {
@@ -19,7 +40,6 @@ export default class Memes extends Component {
       <div className="Memes">
         <header className="Page-header">
           <Navbar/>
-          <p>Memes placeholder</p>
           <img src={`https://i.imgur.com/${this.getRandomId()}.png`} alt='Whoops failed to load there'></img>
         </header>
       </div>
