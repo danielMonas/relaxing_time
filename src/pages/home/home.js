e1e8eaafaf3afadee8953278d9493165ab30395a
danielMonas <danielmonas.12@gmail.com>
2022-01-03 00:29:53 +0200

    patched home page text

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -9,9 +9,9 @@ function Home(props, state) {
         <Navbar/>
         <img src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn170.picsart.com%2Fupscale-235773094028212.png&f=1&nofb=1' className="Home-logo" alt="logo" />
         <p>Hello Shelly! </p>
-        <p>I made you site to relax and watch</p>
-        <p> some funny memes and videos </p>
-        <p>I love you and hope you're alright <span role={"img"}>🧡</span> </p>
+        <p>I made you a website to relax and watch</p>
+        <p>some funny memes and videos </p>
+        <p>I love you so much!<span role={"img"}>🧡</span> </p>
         
       </header>
     </div>
